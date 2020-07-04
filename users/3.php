5167a0ab7c582105c55e25ccdb7e31a841209695
Hana-kuroi <66165198+Hana-kuroi@users.noreply.github.com>
2020-07-04 13:26:50 +0300

    Update 3.php

--- users/3.php
+++ users/3.php
@@ -1,15 +1,26 @@
-<form action = "search.php" method = "post" >
-    <table >
-        <tr >
-            <td > Name:</td >
-            <td ><input type = "text" name = "name"></td >
-        </tr >
-        <tr >
-            <td > Surname:</td >
-            <td ><input type = "text" name = "surname"></td >
-        </tr >
-        <tr >
-            <td><input type = "submit" value = "Search" ></td >
-        </tr >
-    </table >
-</form >
+<head>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script src="script.js"></script>
+</head>
+<body>
+    <form id = "myForm">
+        <select id = role>
+           <table >
+            <tr >
+                <td > Name:</td >
+                <td ><input type = "text" name = "name"></td >
+            </tr >
+                <tr >
+                <td > Surname:</td >
+                <td ><input type = "text" name = "surname"></td >
+            </tr >
+            <tr >
+                <td><input type = "submit" value = "search" ></td >
+            </tr >
+        </table > 
+        </select>
+        <input id = search type = "submit" value = "search" >
+    </form >
+    <div id = "cont"></div>
+</body>
